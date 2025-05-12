7b54eb16ed24b1b4c2f8fa1ef184789ebbdcfd4c
pssvlng <info@passivlingo.com>
2025-05-12 11:56:07 +0200

    bash script elevated priv

--- copy_lang_context.sh
+++ copy_lang_context.sh
@@ -3,17 +3,17 @@
 
 FILE_TO_COPY="docker-compose-$1.yml"
 DESTINATION="docker-compose.yml"
-cp "$FILE_TO_COPY" "$DESTINATION"
+sudo cp "$FILE_TO_COPY" "$DESTINATION"
 echo "Copied '$FILE_TO_COPY' to '$DESTINATION'"
 
 FILE_TO_COPY="backend/post_install_scripts.$1.py"
 DESTINATION="backend/post_install_scripts.py"
-cp "$FILE_TO_COPY" "$DESTINATION"
+sudo cp "$FILE_TO_COPY" "$DESTINATION"
 echo "Copied '$FILE_TO_COPY' to '$DESTINATION'"
 
 FILE_TO_COPY="frontend/src/app/config/app-config.$1.ts"
 DESTINATION="frontend/src/app/config/app-config.ts"
-cp "$FILE_TO_COPY" "$DESTINATION"
+sudo cp "$FILE_TO_COPY" "$DESTINATION"
 echo "Copied '$FILE_TO_COPY' to '$DESTINATION'"
 
 if [ "$1" = "eu" ]; then
@@ -22,20 +22,20 @@ else
     BASE_URL="/wordnet-$1/"
 fi
 FILE="frontend/angular.json"
-sed -i "s|\"baseHref\": \"/\"|\"baseHref\": \"$BASE_URL\"|" "$FILE"
+sudo sed -i "s|\"baseHref\": \"/\"|\"baseHref\": \"$BASE_URL\"|" "$FILE"
 
 echo "Proxy Path '$BASE_URL' set for '$1'."
 
 NETWORK_NAME="lang-network"
-if ! docker network ls | grep -q "$NETWORK_NAME"; then    
-    docker network create "$NETWORK_NAME"
+if ! sudo docker network ls | grep -q "$NETWORK_NAME"; then    
+    sudo docker network create "$NETWORK_NAME"
     echo "Network '$NETWORK_NAME' created."
 else
     echo "Network '$NETWORK_NAME' already exists."
 fi
 
-if ! podman network ls | grep -q "$NETWORK_NAME"; then    
-    podman network create "$NETWORK_NAME"
+if ! sudo podman network ls | grep -q "$NETWORK_NAME"; then    
+    sudo podman network create "$NETWORK_NAME"
     echo "Network '$NETWORK_NAME' created."
 else
     echo "Network '$NETWORK_NAME' already exists."
